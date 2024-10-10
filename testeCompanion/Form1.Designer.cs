f9d2746c7d1d1e9e8ff54630886dc99039ec22ed
MonteiroVideo <166059439+MonteiroVideo@users.noreply.github.com>
2024-10-10 15:01:16 -0300

    Add files via upload

---
+++ testeCompanion/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace testeCompanion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.lblCompanion = new System.Windows.Forms.Label();
+            this.txtCompanion = new System.Windows.Forms.TextBox();
+            this.lblIPPort = new System.Windows.Forms.Label();
+            this.txtHoras = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtMin = new System.Windows.Forms.Label();
+            this.txtSec = new System.Windows.Forms.Label();
+            this.lblTimeString = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(16, 118);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 0;
+            this.btnEnviar.Text = "ENVIAR";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // lblCompanion
+            // 
+            this.lblCompanion.AutoSize = true;
+            this.lblCompanion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCompanion.Location = new System.Drawing.Point(12, 9);
+            this.lblCompanion.Name = "lblCompanion";
+            this.lblCompanion.Size = new System.Drawing.Size(133, 20);
+            this.lblCompanion.TabIndex = 1;
+            this.lblCompanion.Text = "Texto Companion";
+            // 
+            // txtCompanion
+            // 
+            this.txtCompanion.Location = new System.Drawing.Point(16, 79);
+            this.txtCompanion.Name = "txtCompanion";
+            this.txtCompanion.Size = new System.Drawing.Size(100, 20);
+            this.txtCompanion.TabIndex = 2;
+            // 
+            // lblIPPort
+            // 
+            this.lblIPPort.AutoSize = true;
+            this.lblIPPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIPPort.Location = new System.Drawing.Point(346, 0);
+            this.lblIPPort.Name = "lblIPPort";
+            this.lblIPPort.Size = new System.Drawing.Size(82, 20);
+            this.lblIPPort.TabIndex = 3;
+            this.lblIPPort.Text = "IP PORTA";
+            // 
+            // txtHoras
+            // 
+            this.txtHoras.AutoSize = true;
+            this.txtHoras.Location = new System.Drawing.Point(296, 123);
+            this.txtHoras.Name = "txtHoras";
+            this.txtHoras.Size = new System.Drawing.Size(23, 13);
+            this.txtHoras.TabIndex = 4;
+            this.txtHoras.Text = "HH";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(353, 187);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtMin
+            // 
+            this.txtMin.AutoSize = true;
+            this.txtMin.Location = new System.Drawing.Point(366, 123);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(25, 13);
+            this.txtMin.TabIndex = 8;
+            this.txtMin.Text = "MM";
+            // 
+            // txtSec
+            // 
+            this.txtSec.AutoSize = true;
+            this.txtSec.Location = new System.Drawing.Point(436, 123);
+            this.txtSec.Name = "txtSec";
+            this.txtSec.Size = new System.Drawing.Size(21, 13);
+            this.txtSec.TabIndex = 9;
+            this.txtSec.Text = "SS";
+            // 
+            // lblTimeString
+            // 
+            this.lblTimeString.AutoSize = true;
+            this.lblTimeString.Location = new System.Drawing.Point(366, 60);
+            this.lblTimeString.Name = "lblTimeString";
+            this.lblTimeString.Size = new System.Drawing.Size(45, 13);
+            this.lblTimeString.TabIndex = 10;
+            this.lblTimeString.Text = "HORAS";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(25, 202);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 11;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblTimeString);
+            this.Controls.Add(this.txtSec);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtHoras);
+            this.Controls.Add(this.lblIPPort);
+            this.Controls.Add(this.txtCompanion);
+            this.Controls.Add(this.lblCompanion);
+            this.Controls.Add(this.btnEnviar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Label lblCompanion;
+        private System.Windows.Forms.TextBox txtCompanion;
+        private System.Windows.Forms.Label lblIPPort;
+        private System.Windows.Forms.Label txtHoras;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label txtMin;
+        private System.Windows.Forms.Label txtSec;
+        private System.Windows.Forms.Label lblTimeString;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ testeCompanion/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace testeCompanion
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener tcpListener;
+        private Thread tcpListenerThread;
+        private TcpClient tcpClient;
+        private int port = 7600; // Porta para escutar comandos
+        private string companionIP = IPAddress.Loopback.ToString();
+        private int companionPort; // Porta do Companion
+        private System.Windows.Forms.Timer timer;
+        private DateTime startTime;
+        private bool isTimerRunning;
+        string custom;
+
+        public Form1()
+        {
+            InitializeComponent();
+            companionPort = 12345;
+            companionIP = GetLocalIPAddress();
+            lblIPPort.Text = $"IP: {companionIP}, Porta: {port}";
+            StartTCPListener();
+            InitializeTimer();
+        }
+
+        private string GetLocalIPAddress()
+        {
+            string hostName = Dns.GetHostName();
+            IPAddress[] localIPs = Dns.GetHostAddresses(hostName);
+
+            foreach (IPAddress address in localIPs)
+            {
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return address.ToString();
+                }
+            }
+
+            return "127.0.0.1"; // Retorna loopback caso nenhum IP válido seja encontrado
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Adicionando 5 itens ao ListBox ao carregar o formulário
+            listBox1.Items.Add("Item 1");
+            listBox1.Items.Add("Item 2");
+            listBox1.Items.Add("Item 3");
+            listBox1.Items.Add("Item 4");
+            listBox1.Items.Add("Item 5");
+            listBox1.Items.Add("Item 6");
+            listBox1.Items.Add("Item 7");
+            listBox1.Items.Add("Item 8");
+            listBox1.Items.Add("Item 9");
+            listBox1.Items.Add("Item 10");
+            listBox1.Items.Add("Item 11");
+            listBox1.Items.Add("Item 12");
+            listBox1.Items.Add("Item 13");
+            listBox1.Items.Add("Item 14");
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1000; // Atualiza a cada segundo
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (isTimerRunning)
+            {
+                TimeSpan elapsedTime = DateTime.Now - startTime;
+                string hours = elapsedTime.Hours.ToString("D2");
+                string minutes = elapsedTime.Minutes.ToString("D2");
+                string seconds = elapsedTime.Seconds.ToString("D2");
+
+                txtHoras.Text = hours;
+                txtMin.Text = minutes;
+                txtSec.Text = seconds;
+
+                string timeString = $"{hours}:{minutes}:{seconds}";
+                lblTimeString.Text = timeString; // Label que mostra o tempo completo
+
+                SendTimeToCompanion(hours, minutes, seconds, timeString);
+            }
+        }
+
+        private void StartTCPListener()
+        {
+            try
+            {
+                tcpListener = new TcpListener(IPAddress.Any, port);
+                tcpListener.Start();
+                tcpListenerThread = new Thread(new ThreadStart(ListenForClients));
+                tcpListenerThread.IsBackground = true;
+                tcpListenerThread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao iniciar o listener TCP: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ListenForClients()
+        {
+            while (true)
+            {
+                try
+                {
+                    TcpClient client = tcpListener.AcceptTcpClient();
+                    Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClientComm));
+                    clientThread.IsBackground = true;
+                    clientThread.Start(client);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Erro ao aceitar conexão TCP: {ex.Message}");
+                }
+            }
+        }
+
+        private void HandleClientComm(object client_obj)
+        {
+            tcpClient = (TcpClient)client_obj;
+            NetworkStream clientStream = tcpClient.GetStream();
+
+            byte[] message = new byte[4096];
+            int bytesRead;
+
+            while (true)
+            {
+                bytesRead = 0;
+
+                try
+                {
+                    bytesRead = clientStream.Read(message, 0, 4096);
+                }
+                catch
+                {
+                    break;
+                }
+
+                if (bytesRead == 0)
+                {
+                    break;
+                }
+
+                string messageReceived = Encoding.ASCII.GetString(message, 0, bytesRead);
+                Console.WriteLine($"Recebido TCP: {messageReceived}");
+
+                _ = Invoke((MethodInvoker)(() =>
+                {
+                    lblCompanion.Text = messageReceived;
+
+                    if (lblCompanion.Text == "GO")
+                    {
+                        txtHoras.Text = "50";
+                    }
+                }));
+            }
+
+            tcpClient.Close();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            startTime = DateTime.Now;
+            isTimerRunning = true;
+            timer.Start();
+        }
+
+        private void SendTimeToCompanion(string hours, string minutes, string seconds, string timeString)
+        {
+            try
+            {
+                using (TcpClient client = new TcpClient(companionIP, companionPort))
+                using (NetworkStream stream = client.GetStream())
+                {
+                    byte[] buffer = Encoding.ASCII.GetBytes($"hours={hours}&minutes={minutes}&seconds={seconds}&time={timeString}");
+                    stream.Write(buffer, 0, buffer.Length);
+                    stream.Flush();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao enviar mensagem: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void UpdateCompanionWithList()
+        {
+            try
+            {
+                // Criar uma lista temporária para garantir que sempre haja 20 itens
+                var tempList = listBox1.Items.Cast<string>().ToList();
+
+                // Adicionar strings "PRESET" com índice até que a lista tenha 20 itens
+                while (tempList.Count < 20)
+                {
+                    tempList.Add($"PRESET\n{tempList.Count + 1}");
+                }
+
+                // Criar a string de mensagem formatada com base na lista de itens
+                string message = string.Join("&", tempList.Select((item, index) => $"item{index + 1}={item}"));
+
+                // Enviar a mensagem formatada ao Companion
+                SendMessageToCompanion(message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao atualizar o Companion com a lista: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            tcpListener.Stop();
+            tcpListenerThread?.Abort();
+            tcpClient?.Close();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            // Captura os valores de horas e minutos das caixas de texto
+            string hours = txtHoras.Text;
+            string minutes = txtMin.Text;
+
+            // Formata a mensagem no formato esperado pelo Companion
+            string message = $"TIME=+{hours}:{minutes}:00&STATE=PLAYING&DISPLAY=TIMER&MESSAGE=FALSE";
+
+            // Envia a mensagem ao Companion
+            SendMessageToCompanion(message);
+
+            UpdateCompanionWithList();
+        }
+
+        private void SendMessageToCompanion(string message)
+        {
+            try
+            {
+                // Conecta ao Companion usando TCP
+                using (TcpClient client = new TcpClient(companionIP, companionPort))
+                using (NetworkStream stream = client.GetStream())
+                {
+                    // Envia a mensagem
+                    byte[] buffer = Encoding.ASCII.GetBytes(message + "\r\n");
+                    stream.Write(buffer, 0, buffer.Length);
+                    stream.Flush();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao enviar mensagem: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+
+
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Evento de seleção de item, se necessário
+        }
+    }
+}
